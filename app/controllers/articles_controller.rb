7d2406b5f7d1ff0dc6885577d230663a4c355a16
atoder <alexandert@gmail.com>
2015-12-15 18:12:42 -0800

    Loop articles on index

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,30 @@
+class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :destroy]
+
+  def index
+    @articles = Article.all.order('created_at DESC')
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article 
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
